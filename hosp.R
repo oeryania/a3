b084f38ce7831cfd57a0871d13ec55f266d94625
Osama Al-Eryani <oeryania@gmail.com>
2023-08-25 15:54:15 -0400

    takes state and outcome and returns maximum outcome value (with error handling for invalid outcome and state)
    next, need to return name of hospital for maximum outocme

--- hosp.R
+++ hosp.R
@@ -1,1 +1,32 @@
-print("test")
+library(dplyr)
+
+outcomeFilename <- "outcome-of-care-measures.csv"
+varPrefix <- "Hospital.30.Day.Death..Mortality..Rates.from."
+
+best <- function(state, outcome) {
+    outcomes <- read.csv(outcomeFilename, colClasses = "character")
+    varName <- paste(varPrefix, varSuffix(outcome), sep = "")
+    outcomesForState <- filter(outcomes, State == state)
+    if (nrow(outcomesForState) == 0) {
+        cat(sep = "", 'Error in best("', state, '", "', outcome, '") : invalid state')
+        opt <- options(show.error.messages = FALSE)
+        on.exit(options(opt))
+        stop()
+    }
+    outcomeForState <- tryCatch( outcomesForState[, varName],
+        error = function(e) {
+            cat(sep = "", 'Error in best("', state, '", "', outcome, '") : invalid outcome')
+            opt <- options(show.error.messages = FALSE)
+            on.exit(options(opt))
+            stop()
+        }
+    )
+    suppressWarnings(outcomeData <- as.numeric(outcomeForState))
+    max(outcomeData, na.rm = TRUE)
+}
+
+varSuffix <- function(x) {
+    s <- strsplit(x, " ")[[1]]
+    paste(toupper(substring(s, 1, 1)), substring(s, 2),
+          sep = "", collapse = ".")
+}
