24f3dabc40862c81df9924f106e306665a24c8e9
Osama Al-Eryani <oeryania@gmail.com>
2023-08-28 17:24:17 -0400

    replaced rank() with arrange() to respect assignment rules (different rank for ties based on alphabetical appearance)

--- best.R
+++ best.R
@@ -10,14 +10,21 @@ outcome.data <- mutate(read.csv("outcome-of-care-measures.csv", colClasses = "ch
 
 plot.outcome.hist <- function(outcome) hist(outcomeData[, outcome])
 
-best <- function(state, outcome) {
+hospital.ranks <- function(state, outcome) {
     stopifnot("invalid state" = state %in% outcome.data$State)
     stopifnot("invalid outcome" = outcome %in% names(outcome.data))
-    outcome.data.filtered <- outcomeData %>%
+    outcomeDataFilteredOrdered <- outcomeData %>%
         subset(State == state) %>%
         select(Hospital.Name, as.character(outcome)) %>%
-        na.omit
-    outcome.data.filtered.ranked <- cbind(outcome.data.filtered, Rank = dense_rank(outcome.data.filtered[,2]))
-    outcome.data.filtered.ranked.ordered <- arrange(outcome.data.filtered.ranked, Rank, Hospital.Name)
-    outcome.data.filtered.ranked.ordered[1, 1]
+        na.omit %>%
+        arrange(.data[[outcome]], Hospital.Name) %>%
+        cbind(Rank = seq(nrow(.)))
+}
+
+best <- function(state, outcome) {
+    hospital.ranks(state, outcome)[1, 1]
+}
+
+rankhospital <- function(state, outcome, num = "best") {
+    subset(hospital.ranks(state, outcome), Rank == num)
 }
