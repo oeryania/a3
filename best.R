8b8386ce3606dc46ad04a12c415a6734bb06c22c
Osama Al-Eryani <oeryania@gmail.com>
2023-08-30 11:26:27 -0400

    added assignment description

--- best.R
+++ best.R
@@ -1,3 +1,28 @@
+# Assignment Description:
+#
+# Download the file ProgAssignment3-data.zip from:
+# https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2FProgAssignment3-data.zip
+# containing the data for. The data for this assignment come from the Hospital
+# Compare web site (http://hospitalcompare.hhs.gov) run by the U.S. Department
+# of Health and Human Services. The purpose of the web site is to provide data
+# and information about the quality of care at over 4,000 Medicare-certified
+# hospitals in the U.S. This dataset essentially covers all major U.S.
+# hospitals. This dataset is used for a variety of purposes, including
+# determining whether hospitals should be fined for not providing high quality
+# care to patients (see http://goo.gl/jAXFX for some background on this
+# particular topic). The Hospital Compare web site contains a lot of data and we
+# will only look at a small subset for this assignment. The zip file for this
+# assignment contains three files • outcome-of-care-measures.csv: Contains
+# information about 30-day mortality and readmission rates for heart attacks,
+# heart failure, and pneumonia for over 4,000 hospitals. • hospital-data.csv:
+# Contains information about each hospital. • Hospital_Revised_Flatfiles.pdf:
+# Descriptions of the variables in each file (i.e the code book). A description
+# of the variables in each of the files is in the included PDF file named
+# Hospital_Revised_Flatfiles.pdf. This document contains information about many
+# other files that are not included with this programming assignment. You will
+# want to focus on the variables for Number 19 (“Outcome of Care Measures.csv”)
+# and Number 11 (“Hospital Data.csv”).
+
 library(dplyr)
 
 # intilize data by reading outcome-of-care-measures.csv (downloaded from https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2FProgAssignment3-data.zip)
